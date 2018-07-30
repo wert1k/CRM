5bdf471dfee8f1aa1483249f57bcb2ca931eb2e5
filorz <bogdanoff.filipp@yandex.ru>
2018-07-30 14:13:45 +0500

    fix - send mail

--- src/main/java/com/ewp/crm/CrmApplication.java
+++ src/main/java/com/ewp/crm/CrmApplication.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 //@SpringBootApplication
 public class CrmApplication {
@@ -16,7 +17,7 @@ public class CrmApplication {
 	public static void main(String[] args) {
 		IncomeStringToClient incomeStringToClient = new IncomeStringToClient();
 		String parser = "<br>Страница: <b>http://www.java-mentor.com/index.html</b> <br />Форма: <b>Java Test</b> <br />1: <b>2</b> <br />2: <b>3</b> <br />3: <b>3</b> <br />4: <b>3</b> <br />5: <b>2</b> <br />6: <b>3</b> <br />Имя: <b>testew</b> <br />Social 2: <b>https://vk.com/andiovermide</b> <br />Phone 6: <b>89110812259</b> <br />City 6: <b>Россия</b> <br />Email 2: <b>mcdncrnc73@gmail.com</b> <br />";
-		String parser2 = "Страница: http://www.java-mentor.com/index.html Форма: Java Test 1: 2 3: 3 4: 2 5: 3 6: 4 Имя: testew Social 2: https://vk.com/andiovermide Phone 6: 89110812259 City 6: Россия Email 2: mcdncrnc73@gmail.com";
+		String parser2 = "Страница: http://www.java-mentor.com/index.html Форма: Java Test 1: 2 2: 2 3: 3 4: 2 5: 3 6: 4 Имя: testew Social 2: https://vk.com/andiovermide Phone 6: 89110812259 City 6: Россия Email 2: mcdncrnc73@gmail.com";
 		Client client = incomeStringToClient.convert(parser);
 		if (client != null) {
 			//--------------------------
@@ -43,26 +44,32 @@ public class CrmApplication {
                 .replace("Java Test", "")
                 .replaceAll(" \\d+: ", "");
 
-        List result = Arrays.asList("3", "3", "1", "2", "3", "4");
-        List resultList = Arrays.asList(indexQuery.split("\\s*\\s*"));
-		List resultTest = Arrays.asList(listQuery.split("\\s*\\s*"));
+        List<Integer> result = Arrays.asList(3, 3, 1, 2, 3, 4);
+        List<Integer> resultList = Arrays.asList(indexQuery.split("\\s*\\s*")).stream().map(s -> Integer.parseInt(s.trim())).collect(Collectors.toList());
+		List<Integer> resultTest = Arrays.asList(listQuery.split("\\s*\\s*")).stream().map(s -> Integer.parseInt(s.trim())).collect(Collectors.toList());
 
         System.out.println(resultList);
         System.out.println(resultTest);
 
-        for (int q = 0, count = 0, i = 0; i < resultTest.size(); i++) {
-            if (result.get(i).equals(resultTest.get(i + q))) {
-                count++;
-                System.out.println(count);
-                if ((resultTest.size()) - i == 1) {
-                    return (long) ((count * 100) / result.size());
-                }
-            }
-            if ((result.size()) - i == 1 & count == 0) {
-                return null;
-            } else {
-                ++q;
-            }
+        for (int q = 1, count = 0, i = 0; i < result.size(); i++) {
+			if (resultList.get(i).equals(i + q)) {
+				if (resultList.get(i + 1).equals(i + q + 1)) {
+					if (result.get(i).equals(resultTest.get(i))) {
+						count++;
+						System.out.println(count);
+						if ((resultTest.size()) - i == 1) {
+							return (long) ((count * 100) / result.size());
+						}
+					}
+					if ((result.size()) - i == 1 & count == 0) {
+						return null;
+					}
+				} else {
+					q++;
+				}
+
+			}
+
         }
         return 0L;
 	}
